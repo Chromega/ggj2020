b52b712799f866d70d9f181cddbc96646044771a
Sean Liu (AR/VR) <sean.y.liu@gmail.com>
2020-02-01 00:31:05 -0800

    updating port

--- node/index.js
+++ node/index.js
@@ -1,8 +1,9 @@
 var static = require('node-static');
 var http = require('http');
+const PORT = process.env.PORT || 5000
 
 var file = new(static.Server)();
 
 http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(8080);
+}).listen(PORT);
