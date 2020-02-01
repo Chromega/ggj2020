5129c30f7eb7a44b193f84acf59ce9691bb78dbe
Mark Sullivan <markasullivaniii@gmail.com>
2020-02-01 14:35:50 -0800

    deletions

--- Assets/RealMatter/Rendering/DeformableEdge.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class RMDeformableEdge {
-    //RMDeformableVertexInfo[] v = new RMDeformableVertexInfo[2];
-    //List<RMDeformableTriangle> connectedTriangles = new List<RMDeformableTriangle>();
-}
--- Assets/RealMatter/Scripts/FractureScripts/FractureScript.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public abstract class FractureScript : MonoBehaviour {
-
-    public abstract void OnFracture(RMBody.FractureInfo info);
-}
