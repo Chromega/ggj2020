b63f631695a3acc686fa3b40d45e7d88503825c3
Mark Sullivan <markasullivaniii@gmail.com>
2020-02-01 11:02:57 -0800

    webgl build script

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Builder
+{
+
+    [MenuItem("GGJ/Build WebGL")]
+    public static void BuildWebGL()
+    {
+
+        // Get filename.
+        string path = "Builds-Public/phone";////EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] { "Assets/Scenes/Lobby.unity", "Assets/Scenes/PhoneScene.unity" };
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path, BuildTarget.WebGL, BuildOptions.None);
+
+    }
+}
