6294d3184c9610a44465aabee3d5c5fc812be989
James Shieh <randomletters13@gmail.com>
2020-02-01 13:25:20 -0800

    update ship and item spawn

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,6 +6,12 @@ public class ItemSpawner : MonoBehaviour
 {
     public List<Item> items;
 
+    const int X_BOUND = 37 * 7;
+    const int Z_BOUND = 17 * 7;
+
+    const float X_OFFSET = 1.5f;
+    const float Z_OFFSET = -1.2f;
+
     void Awake()
     {
         items = new List<Item>();
@@ -28,7 +34,9 @@ public class ItemSpawner : MonoBehaviour
     public void SpawnItem(Item item)
     {
         Pickupable pick = PickupableFactory.Instance.Activate(item);
-        pick.transform.position = new Vector3(0f, 0f, 0f);
+        float x = (Random.Range(0, X_BOUND) - (X_BOUND / 2)) / 10.0f + X_OFFSET;
+        float z = (Random.Range(0, Z_BOUND) - (Z_BOUND / 2)) / 10.0f + Z_OFFSET;
+        pick.transform.position = new Vector3(x, 1f, z);
     }
 
     public Item PickRandomItem()
