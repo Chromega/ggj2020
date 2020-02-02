532e1509b83e37c852388b55919135272ba87aee
Sean Liu (AR/VR) <sean.y.liu@gmail.com>
2020-02-01 21:25:07 -0800

    adding a second audio source

---
+++ Assets/Scripts/Audio/AttemptRepairAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttemptRepairAudio : MonoBehaviour
+{
+    public AudioClip wrongRepairClip;
+    public AudioClip rightRepairClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playWrongRepair()
+	{
+
+	}
+
+    void playRightRepaitClip()
+	{
+
+	}
+}
