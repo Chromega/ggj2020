6c85eebb821d7e7aa377229e0c5f0c71adf94630
Sean Liu (AR/VR) <sean.y.liu@gmail.com>
2020-02-01 20:11:42 -0800

    fix phone buttons

--- Assets/Scripts/Networking/WebSceneMgr.cs
+++ Assets/Scripts/Networking/WebSceneMgr.cs
@@ -24,25 +24,36 @@ public class WebSceneMgr : MonoBehaviour
     public void updateInventoryButtons(string[] inv)
     {
       // instantiate enough buttons
+      /* buttons are pre-instantiated
       for (var i = buttonList.Count; i < inv.Length; i++)
       {
-        Button button = Instantiate(buttonPrefab, panelToAttach.transform, false);
-        button.transform.position = panelToAttach.transform.position;
-        Vector3 currentPos = button.transform.position;
-        currentPos.y += -50 + i*60;
-        button.transform.position = currentPos;
-        //button.GetComponent<RectTransform>().SetParent(panelToAttach.transform);
-        int buttonIdx = i;
-        button.onClick.AddListener(() => {
-          NetPlayerController.LocalInstance.UseItem(buttonIdx);
-        });
-        buttonList.Add(button);
-      }
+            Button button = Instantiate(buttonPrefab, panelToAttach.transform, false);
+            button.transform.position = panelToAttach.transform.position;
+            Vector3 currentPos = button.transform.position;
+            currentPos.y += -50 + i*60;
+            button.transform.position = currentPos;
+            //button.GetComponent<RectTransform>().SetParent(panelToAttach.transform);
+        
+            buttonList.Add(button);
+      }*/
 
       for (var i = 0; i < inv.Length; i++)
       {
-        Button button = buttonList[i];
-        button.GetComponentInChildren<TextMeshProUGUI>().text = inv[i];
+            Button button = buttonList[i];
+            button.GetComponentInChildren<TextMeshProUGUI>().text = inv[i];
+            int buttonIdx = i;
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => {
+                NetPlayerController.LocalInstance.UseItem(buttonIdx);
+            });
+            if (inv[i] != "")
+            {
+                button.gameObject.SetActive(true);
+            } else
+            {
+                button.gameObject.SetActive(false);
+            }
+            
       }
 
     }
