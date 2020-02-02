a770978b977d2218756891f9d2cddd6ec10d5f27
Sean Liu (AR/VR) <sean.y.liu@gmail.com>
2020-02-01 17:49:16 -0800

    audio

---
+++ Assets/Scripts/Audio/IntroThenLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class IntroThenLoop : MonoBehaviour
+{
+    public AudioClip startClip;
+    public AudioClip loopClip;
+    void Start()
+    {
+        GetComponent<AudioSource>().loop = true;
+        StartCoroutine(playSound());
+    }
+
+    IEnumerator playSound()
+    {
+        GetComponent<AudioSource>().clip = startClip;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        GetComponent<AudioSource>().clip = loopClip;
+        GetComponent<AudioSource>().Play();
+    }
+}
